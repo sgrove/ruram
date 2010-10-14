fa119171cad4d2a81da00fa5fbbf9017090df9f8
Jan Stępień <jstepien@users.sourceforge.net>
2010-10-15 01:40:14 +0200

    Catching exceptions

--- webapp/webapp.rb
+++ webapp/webapp.rb
@@ -8,11 +8,15 @@ class WebApp < Sinatra::Base
   post '/run' do
     reply = nil
     t = Thread.new do
-      vm = VM.new Parser.new(Lexer.new).parse params[:code] + "\n"
-      vm.execute
-      reply = vm.registers.map do |pair|
-        "#{pair[0]}\t#{pair[1]}"
-      end.join "\n"
+      begin
+        vm = VM.new Parser.new(Lexer.new).parse params[:code] + "\n"
+        vm.execute
+        reply = vm.registers.map do |pair|
+          "#{pair[0]}\t#{pair[1]}"
+        end.join "\n"
+      rescue Object => bang
+        reply = "ERROR: #{bang.to_s}"
+      end
     end
     if not t.join 5
       t.kill
