aac2aa4e11c5aea59aa05e98e4c62dadd795612a
Jan Stępień <jstepien@users.sourceforge.net>
2010-10-15 01:31:14 +0200

    A time limit (5s)

--- webapp/webapp.rb
+++ webapp/webapp.rb
@@ -6,11 +6,19 @@ require 'haml'
 
 class WebApp < Sinatra::Base
   post '/run' do
-    vm = VM.new Parser.new(Lexer.new).parse params[:code] + "\n"
-    vm.execute
-    vm.registers.map do |pair|
-      "#{pair[0]}\t#{pair[1]}"
-    end.join "\n"
+    reply = nil
+    t = Thread.new do
+      vm = VM.new Parser.new(Lexer.new).parse params[:code] + "\n"
+      vm.execute
+      reply = vm.registers.map do |pair|
+        "#{pair[0]}\t#{pair[1]}"
+      end.join "\n"
+    end
+    if not t.join 5
+      t.kill
+      reply = 'ERROR: code execution time limit hit'
+    end
+    reply
   end
 
   get '/' do
