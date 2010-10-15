770a25fb50da4a4882a2f083f9637fe947c8a2e6
Jan Stępień <jstepien@users.sourceforge.net>
2010-10-15 09:44:55 +0200

    Catching output generated by Log on stderr

--- webapp/webapp.rb
+++ webapp/webapp.rb
@@ -3,24 +3,31 @@ require 'lexer'
 require 'parser'
 require 'vm'
 require 'haml'
+require 'stringio'
 
 class WebApp < Sinatra::Base
   post '/run' do
     reply = nil
     t = Thread.new do
       begin
+        $stderr = StringIO.new
         vm = VM.new Parser.new(Lexer.new).parse params[:code] + "\n"
         vm.execute
         reply = vm.registers.map do |pair|
           "#{pair[0]}\t#{pair[1]}"
         end.join "\n"
-      rescue Object => bang
-        reply = "ERROR: #{bang.to_s}"
+      rescue SystemExit
+        $stderr.rewind
+        reply = $stderr.read
+      rescue => bang
+        reply = "Error: #{bang.to_s}"
+      ensure
+        $stderr = STDERR
       end
     end
     if not t.join 5
       t.kill
-      reply = 'ERROR: code execution time limit hit'
+      reply = 'Error: code execution time limit hit'
     end
     reply
   end
